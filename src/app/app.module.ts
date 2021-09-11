e29755edaf00f3fd011d1c96f71d9323cb3d7edb
Kamran777 <kshukurov1@gmail.com>
2021-09-12 01:44:20 +0200

    feat: [Branch-5] created table and got data from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { BookTableComponent } from './components/books/book-table/book-table.component';
+import { BookComponent } from './components/books/book/book.component';
+import { SharedModule } from './shared/shared.module';
+import { TabMenuComponent } from './components/tab-menu/tab-menu.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,6 +16,16 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule
+    AppComponent,
+    TabMenuComponent,
+    BookComponent,
+    BookTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/books/book-table/book-table.component.ts
@@ -0,0 +1,44 @@
+import { Book } from './../../../models/book.model';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-book-table',
+  templateUrl: './book-table.component.html',
+  styleUrls: ['./book-table.component.scss']
+})
+export class BookTableComponent implements OnInit, OnDestroy {
+
+  public subscription: Subscription;
+  public disableRouterLink: boolean = true;
+  @Input() public books: Book[];
+  @Input() public showLoader: boolean = false;
+  @Input() public title: string = 'BOOK TABLE';
+
+  constructor(private apiService: ApiService) {}
+
+  public ngOnInit(): void {
+    this.showLoader = true;
+    if (!this.books) {
+      this.getAllBooks();
+    }
+  }
+
+  public getAllBooks(): void {
+    this.subscription = this.apiService
+      .getAll<Book[]>(`books`)
+      .subscribe((books: any) => {
+        setTimeout(() => {
+          this.showLoader = false;
+          this.books = books;
+        }, 1000);
+      });
+  }
+
+  public ngOnDestroy(): void {
+    if(this.subscription){
+      this.subscription?.unsubscribe();
+    }
+  }
+}
